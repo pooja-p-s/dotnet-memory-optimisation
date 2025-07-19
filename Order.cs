4f3b4b197a6442afac5ec6a3af3b4ebf2a1eeffc
pooja-p-s <sct18cs047@sctce.ac.in>
2025-07-19 19:38:48 +0530

    Initialising project

---
+++ Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TradingPlatform
+{
+	public struct Order
+	{
+		public int Id { get; set; }
+
+		public double Price { get; set; }
+
+		public int Quantity { get; set; }
+
+		public bool IsBuyOrder { get; set; }
+
+	}
+}
+
